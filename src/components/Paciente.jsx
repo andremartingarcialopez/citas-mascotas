2ac230b925c1a046a0f563811433c999bb2105a7
Andy <andy_garlop4@hotmail.com>
2022-05-25 14:38:20 -0500

    Iconos en botones

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,10 +1,10 @@
-const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
-    const { nombre, propietario, email, fecha, sintomas, id } = paciente
+const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
+    const { nombre, propietario, email, fecha, sintomas, id } = paciente
 
     const handleEliminar = () => {
         const respuesta = confirm('Deseas eliminar este paciente?');
 
-        if(respuesta) {
+        if (respuesta) {
             eliminarPaciente(id)
         }
     }
@@ -32,17 +32,29 @@ const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
             </p>
 
             <div className="flex md:justify-between justify-center mt-10 gap-2">
-                <button 
-                    type="button"
-                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
-                    onClick={() => setPaciente(paciente)}
-                >Editar</button>
 
-                <button 
+                <button
                     type="button"
-                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                    className="py-2 flex justify-center gap-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => setPaciente(paciente)}
+                >
+                    <p>Editarr</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 flex" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                    </svg>
+                </button>
+
+
+                <button
+                    type="button"
+                    className="py-2 px-10 flex gap-2 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
                     onClick={handleEliminar}
-                >Eliminar</button>
+                >
+                    <p>Eliminar</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </button>
             </div>
         </div>
     )
