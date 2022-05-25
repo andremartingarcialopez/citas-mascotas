d779344b1dbdfc55b97bcbfebd89b1cdce30fb1b
Andy <andy_garlop4@hotmail.com>
2022-05-25 14:52:56 -0500

    detalle en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
                 Happy :{""} 
                 <span className="text-indigo-700">) Pet </span>
             </h1>
-            <p className="text-lg mt-5 text-center mb-10">Agenda una cita por el bien de tu <span className="text-indigo-600 font-bold">Mascota</span></p>
+            <p className="text-lg mt-5 text-center mb-10">Con el objetivo de ver a los animales <span className="text-indigo-600 font-bold">Felices</span></p>
         </>
     )
 }
